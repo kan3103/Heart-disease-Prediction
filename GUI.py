5b25c25b405f28b348b2f1f557212068d47340de
Btbach25 <btbach25@gmail.com>
2025-04-28 13:36:57 +0700

    FIx : fix scaler in ANN prepare

--- GUI.py
+++ GUI.py
@@ -94,7 +94,8 @@ if submitted:
         
         # Dự đoán
         if model_choice == "ANN":
-            input_df_scaled = StandardScaler().fit_transform(input_df)
+            scaler = joblib.load('scaler/scaler.pkl')
+            input_df_scaled = scaler.transform(input_df)
             prediction = ann_model.predict(input_df_scaled)
             probability = prediction[0][0]
         elif model_choice == "Naive Bayes":
